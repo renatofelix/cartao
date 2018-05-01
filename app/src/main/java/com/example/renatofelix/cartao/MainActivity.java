b82fcdc488c042f634ba5cbf49ac852d63e725e7
renatofelix <renatofelix@gmail.com>
2018-05-01 15:09:13 -0300

    first commit

---
+++ app/src/main/java/com/example/renatofelix/cartao/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.renatofelix.cartao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_sair = (Button) findViewById(R.id.btn_sair);
+        btn_sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+}
